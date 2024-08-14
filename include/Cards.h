848b49c50d89354a325e3a910244b79444c69d70
Calebf2424 <97080069+Calebf2424@users.noreply.github.com>
2024-08-14 11:59:40 -0600

    Update Cards.h

--- include/Cards.h
+++ include/Cards.h
@@ -1,5 +1,34 @@
 #ifndef CARDS_H
 #define CARDS_H
+#include <vector>
+
+class Card {
+ public:
+    Card(char s, int v) : suit(s), value(v) {}
+    void displayCardInfo();
+    char getSuit();
+    int getValue();
+
+ private:
+    char suit;
+    int value;
+};
+
+class CardDeck {
+ public:
+    cardDeck() : deck(52) {};
+    void createDeck();
+    void shuffleDeck();
+    void giveToDealerHand();
+    void giveToPlayerHand();
+    void resetDeck();
+    CardDeck~();
+ private:
+    std::vector<Card*> deck;
+    std::vector<Card*> dealer;
+    std::vector<Card*> player;
+};
+
 
 
 

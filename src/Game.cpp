24aa6b2421f4fc5c9ef83d1bce9e5bbd843f5243
Caleb Farnsworth <97080069+Calebf2424@users.noreply.github.com>
2024-08-16 15:49:41 -0600

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,1 +1,28 @@
 #include "Game.h"
+#include "Cards.h"
+
+Game::Game() : deck(nullptr), _playerScore(0), _dealerScore(0){}
+Game::~Game() {if (deck) delete deck;}
+void Game::start() {
+    deck = new CardDeck();
+}
+
+void Game::runGame() {
+
+}
+
+void Game::playAgain() {
+
+}
+
+void Game::quit() {
+    
+}
+
+void Game::handleInput() {
+
+}
+
+void Game::updateBoard() {
+
+}

b0a67d8927d9446091bd3e73a17df405d7d40b97
Caleb Farnsworth <97080069+Calebf2424@users.noreply.github.com>
2024-08-26 09:04:37 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
-#include <iostream>
-#include "Game.h"
-#include "Cards.h"
+#include <SFML/Graphics.hpp>
 
-int main() {
-    Game* game = new Game();
-    game->start();
-    game->quit();
+int main()
+{
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
 
-    game = nullptr;
-    return 0;
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
-
